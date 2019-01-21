d76393a15eab70b5baebcd2df3941bb8b448e4df
grantgold <grantagold@gmail.com>
2019-01-21 00:46:53 -0500

    day 11

--- site/accounts/grantgold.php
+++ site/accounts/grantgold.php
@@ -7,8 +7,7 @@ email: grantagold@gmail.com
 language: en
 role: admin
 history:
-  - blog/day-10-kagawa-prefecture-japan
+  - blog/day-11-xinyang-china
   - blog/day-9-broken-hill-australia
   - blog/day-8-kuril-islands-russia
   - blog/day-7-haast-new-zealand
-  - blog/day-6-mutalau-niue
--- site/templates/home.php
+++ site/templates/home.php
@@ -11,6 +11,15 @@
           <article class="article grid-container">
 
             <?php snippet('coverimage', $article) ?>
+            <?php
+            // Images for the "project" template are sortable. You
+            // can change the display by clicking the 'edit' button
+            // above the files list in the sidebar.
+            foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
+              <figure>
+                <img src="<?= $image->url() ?>" alt="<?= $page->title()->html() ?>" />
+              </figure>
+            <?php endforeach ?>
 
             <header class="article-header">
               <h2 class="article-title">
