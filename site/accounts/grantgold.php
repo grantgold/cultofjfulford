ba66762a1c5cb93e9c714494dfdd69d876d5294f
grantgold <grantagold@gmail.com>
2019-01-22 05:59:40 -0500

    day 12

--- site/accounts/grantgold.php
+++ site/accounts/grantgold.php
@@ -7,6 +7,7 @@ email: grantagold@gmail.com
 language: en
 role: admin
 history:
+  - blog/day-12-west-sumatra-indonesia
   - blog/day-11-xinyang-china
   - blog/day-9-broken-hill-australia
   - blog/day-8-kuril-islands-russia
